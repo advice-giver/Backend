c4cab5c3933f81026c6e2825a12b492d7a8c4217
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-25 13:08:13 -0500

    added ability to update, delete, and find users

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const server = express();
 const messageRoute = require('../routes/messageRoute.js');
 const postAdvice = require('../routes/postAdviceRoute.js')
 const authRoute = require('../auth/auth-router.js')
-
+const userRoute = require('../routes/userRouter.js')
 
 server.use(express.json());
 server.use(helmet());
@@ -18,5 +18,6 @@ server.use(cors());
 server.use('/messages', messageRoute);
 server.use('/auth', authRoute);
 server.use('/advice', postAdvice);
+server.use('/users', userRoute);
 
 module.exports = server;
