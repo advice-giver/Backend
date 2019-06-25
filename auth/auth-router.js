51974b1441e454921d1ad9ac289d16ab11d11722
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-25 15:27:42 -0500

    fixed bug in login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -39,7 +39,7 @@ router.post('/login', (req,res) => {
     let { username, password } = req.body;
 
     userModel
-    .find(username)
+    .findUser(username)
     .then( user => {
         if (user && bcrypt.compareSync(password, user.password)){
             const token = generateToken(user);//generates token
--- database/migrations/20190624150635_messages.js
+++ database/migrations/20190624150635_messages.js
@@ -19,6 +19,7 @@ exports.up = function(knex, Promise) {
     })
 };
 
+
 exports.down = function(knex, Promise) {
     return knex.schema.dropTableIfExists('messages')
 };
